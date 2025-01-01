d88b8c0fe171d423dc7b3ef63ac86c014f1ab97f
Jarrod Whitley <jarrodwhitley@gmail.com>
2025-01-01 11:53:22 -0600

    xcode updates

--- lib/src/screens/about.dart
+++ lib/src/screens/about.dart
@@ -10,13 +10,20 @@ class AboutScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Settings',
+        title: const Text('About',
             style: TextStyle(
               color: Colors.white,
             )),
         backgroundColor: isAm
             ? const Color.fromARGB(255, 103, 189, 178)
             : const Color.fromARGB(255, 55, 30, 83),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          color: Colors.white,
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
       ),
       body: const Center(
         child: Text('This is the about screen.'),
